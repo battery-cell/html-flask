bc218aa2bd7f0c7f6405ac27ec6788febc2f7ee0
battery-cell <leon.dembowski@infotech.edu.pl>
2025-04-01 21:19:24 +0200

    real

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+def my_function():
+    print("okay bud")
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    message = ""
+    if request.method == 'POST':
+        message = my_function()  # Call the Python function
+    if request.method == 'POST':
+        message = my_function()  # Call the Python function
+    return render_template('index.html', message=message)
 if __name__ == '__main__':
-    app.run()
-print("aaa")
+    app.run(debug=True)
