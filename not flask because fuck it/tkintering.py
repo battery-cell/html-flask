5f9e129b79208fcdc611fad46eb4d49dd7940fa3
battery-cell <leon.dembowski@infotech.edu.pl>
2025-04-02 09:54:54 +0200

    tymon bedzie bity

--- not flask because fuck it/tkintering.py
+++ not flask because fuck it/tkintering.py
@@ -4,7 +4,14 @@ windowed = Tk()
 windowed.title("hello world")
 windowed.geometry("400x400")
 def definition():
-    messagebox.showwarning("Warning")
+    messagebox.showinfo("Warning", "tymon bedziwe bity")
+def definition2():
+    button3.place(x=150,y=40)
+def definition3():
+    messagebox.showerror("kurwo jebana", "tymon to szmata")
 button1 = Button(windowed, text="click me",command=definition)
+button2 = Button(windowed, text="yo",command=definition2)
+button3 = Button(windowed, text="NIGGA", command=definition3)
 button1.place(x=150,y=0)
+button2.place(x=150, y=100)
 windowed.mainloop()
