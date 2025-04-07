b24fa4e569eb383d67a8ef79fe555aa9d50f4cb6
battery-cell <leon.dembowski@infotech.edu.pl>
2025-04-07 14:01:13 +0200

    HAI

--- not flask because fuck it/tkintering.py
+++ not flask because fuck it/tkintering.py
@@ -8,10 +8,10 @@ def definition():
 def definition2():
     button3.place(x=150,y=40)
 def definition3():
-    messagebox.showerror("kurwo jebana", "tymon to szmata")
+    messagebox.showerror("it is what it is", "tymon to dobry bardzo kolega")
 button1 = Button(windowed, text="click me",command=definition)
 button2 = Button(windowed, text="yo",command=definition2)
-button3 = Button(windowed, text="NIGGA", command=definition3)
+button3 = Button(windowed, text="hello", command=definition3)
 button1.place(x=150,y=0)
 button2.place(x=150, y=100)
 windowed.mainloop()
