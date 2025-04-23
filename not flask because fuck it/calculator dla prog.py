f6aa6eb16c61347bff020f8f6d115679f0b9ff8f
battery-cell <leon.dembowski@infotech.edu.pl>
2025-04-23 23:59:35 +0200

    oki komorowski sie bedzie cieszyc (oby)

--- not flask because fuck it/calculator dla prog.py
+++ not flask because fuck it/calculator dla prog.py
@@ -33,33 +33,27 @@ window = Tk()
 window.geometry("500x500")
 window.title("Kalkulator grubastwa")
 
-# Wybór płci
 rad1 = Radiobutton(window, value=1, text="Mężczyzna")
 rad1.grid(row=1, column=1)
 rad2 = Radiobutton(window, value=2, text="Kobieta")
 rad2.grid(row=1, column=2)
 
-# Pole na wagę
 Entry1 = Spinbox(window, from_=1, to=300)
 Entry1.grid(row=2, column=2)
 label1 = Label(window, text="Twoje KG")
 label1.grid(row=2, column=1)
 
-# Pole na wzrost
 Entry2 = Spinbox(window, from_=0.5, to=2.5, increment=0.01)
 Entry2.grid(row=3, column=2)
 label2 = Label(window, text="Twój wzrost w M")
 label2.grid(row=3, column=1)
 
-# Przycisk do obliczenia BMI
 button1 = Button(text="Skalkulatuj BMI", command=oblicz_bmi)
 button1.grid(row=4, column=1)
 
-# Przycisk resetujący
 button2 = Button(text="Resetuj", command=resetuj)
 button2.grid(row=4, column=2)
 
-# Etykieta na wynik
 wynik_label = Label(window, text="", font=("Arial", 14))
 wynik_label.grid(row=5, column=1, columnspan=2)
 
